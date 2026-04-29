5b5d75a15030e058ca8311600e70a6f8742376e4
cbuijs <cbuijs@chrisbuijs.com>
2026-04-29 12:21:37 +0200

    Update 20260429-122137-CEST

--- dummy.go
+++ dummy.go
@@ -1,2 +1,2 @@
-// Dummy, so (unnecessary) errors around modules/etc will not appear to keep go-work and tools sepperatei,
-// and still have shared functions in ./shared.0
+// Dummy, so (unnecessary) errors around modules/etc will not appear to keep go-work and tool builds sepperated,
+// and still have shared functions in ./shared.
--- go.mod
+++ go.mod
@@ -1,3 +1,6 @@
+// Dummy, so (unnecessary) errors around modules/etc will not appear to keep go-work and tool builds sepperated,
+// and still have shared functions in ./shared.
+
 module aggrip-go-root
 
 go 1.26.1
